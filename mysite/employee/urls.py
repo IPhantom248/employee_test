7c14b699cf21a2ac78ee5820b0cb01946ae356e1
Alex <iphantom2510@gmail.com>
2021-10-04 04:20:55 +0300

    Добавил nestable.js, style.css, красивое отображение дерева с dran-n-drop

--- mysite/employee/urls.py
+++ mysite/employee/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from employee.views import get_all_employees
+from employee.views import get_all_employees, get_all_employees_test
 
 urlpatterns = [
-    path('', get_all_employees)
+    path('', get_all_employees),
+    path('test/', get_all_employees_test)
 ]
