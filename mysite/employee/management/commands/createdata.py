9fa1eec0c6f318311a3b1f0ff1076cb584baf734
Grigoriy <SirGrgorytoday@gmail.com>
2021-10-04 23:58:01 +0300

    createdata исправлено

--- mysite/employee/management/commands/createdata.py
+++ mysite/employee/management/commands/createdata.py
@@ -8,10 +8,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fake = Faker()
-        for i in range(0, 10000):
+        for i in range(0, 100):
             rand_level = random.randint(0, 3)  # По заданию необходимо ограничить дерево 5 уровнями
             if rand_level == 0:
-                random_parent = None  # На 0 уровне нет родителя
+                if random.randint(0, 1):
+                    random_parent = None  # На 0 уровне нет родителя
+                else:
+                    random_parent_queryset = EmployeeTree.objects.filter(level=rand_level)
+                    random_parent = random_parent_queryset[random.randint(0, random_parent_queryset.count()-1)]
             else:
                 random_parent_queryset = EmployeeTree.objects.filter(level=rand_level)
                 random_parent = random_parent_queryset[random.randint(0, random_parent_queryset.count()-1)]
@@ -19,6 +23,7 @@ class Command(BaseCommand):
                                         salary=random.randint(1000, 5000), parent=random_parent)
             if not i % 10:
                 print(i)
+        print("Done!")
 
         # print(random_parent.parent)
         # EmployeeTree.objects.create(full_name=fake.name(), level=random.randint(1, 5), hired_at=fake.date_time(),
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employee.apps.EmployeeConfig',
-    'django_seed',
     'mptt'
 
 ]
