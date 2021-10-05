a29c57702b1ff157ad2c862e16e397c77288988b
Alex <example@gmail.com>
2021-10-05 09:37:02 -0400

    Добавлен поиск по всем полям

--- mysite/employee/views.py
+++ mysite/employee/views.py
@@ -1,6 +1,12 @@
+import datetime
+import parser
+
 from django.shortcuts import render
 from employee.models import EmployeeTree
 from django.views.generic import ListView
+from django.db.models import Q
+
+from dateutil.parser import parse
 
 
 def get_all_employees(request):
@@ -17,17 +23,21 @@ class EmployeesListView(ListView):
     template_name = "employee/employees.html"  # <app>/<model>_<viewtype>.html
     context_object_name = "employees"
     ordering = ["full_name"]
-    # paginate_by = 6
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context["title"] = "Sharu X2"
-    #     return context
+    def get_queryset(self):
+        if self.request.GET.get('s'):
+            search = self.request.GET.get('s')
+            # https://docs.djangoproject.com/en/3.2/topics/db/queries/
+            if search.isdigit():
+                return super().get_queryset().filter(Q(pk=search) | Q(level=search) | Q(salary=search))
+            elif search.isalpha() or ''.join(search.split()).isalpha():  # Либо введено имя/фамилия, либо и то и то
+                return super().get_queryset().filter(full_name__icontains=search)
+            else:  # Сюда попадаем если search содержит знаки
+                try:
+                    date = parse(search).date()
+                    return super().get_queryset().filter(hired_at=date)
+                except parser.ParserError:
+                    pass
+        else:
+            return super().get_queryset()
 
-
-
-# full_name = models.CharField(max_length=150)
-# level = models.IntegerField()
-# hired_at = models.DateField()
-# salary = models.IntegerField()
-# parent = TreeForeignKey('self', null=True, blank=True, related_name='children', on_delet
